c10069686861dba602466638fe7a9de5cc328136
QinDie <62633170+QinDie@users.noreply.github.com>
2020-05-12 23:30:16 -0400

    Delete Is a number prime (2) .py

--- Is a number prime (2) .py
+++
@@ -1,9 +0,0 @@
-from Is a number prime  import prime number 
-
-
-a  = int(input('Please Input an Positive Int Number Greater Than One :'))
-b = prime number(a)
-if b:
-    print('is a Prime Number')
-else:
-    print(' is not a Prime Number')
