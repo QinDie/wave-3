278618ae7033a3d6e47df337758ea5885962051d
QinDie <62633170+QinDie@users.noreply.github.com>
2020-05-12 23:30:24 -0400

    Delete Is a number prime .py

--- Is a number prime .py
+++
@@ -1,8 +0,0 @@
-def prime number(a) :
-    if a >= 2:
-        for i in range(2,a):
-            if a%i==0 :
-                return False
-        return True
-    else : 
-        return'invalid value'
